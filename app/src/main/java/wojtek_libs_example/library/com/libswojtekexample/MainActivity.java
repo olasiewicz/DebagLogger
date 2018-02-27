fb3de4858b1726c80f74298f859a863c4f97f2f2
Olasiewicz <wojciech.olasiewicz@mobica.com>
2018-02-27 15:05:46 +0100

    Git init

---
+++ app/src/main/java/wojtek_libs_example/library/com/libswojtekexample/MainActivity.java
@@ -0,0 +1,13 @@
+package wojtek_libs_example.library.com.libswojtekexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ debugutil/src/main/java/wojtek_libs_example/library/com/debugutil/DebugLog.java
@@ -0,0 +1,8 @@
+package wojtek_libs_example.library.com.debugutil;
+
+/**
+ * Created by wool on 2018-02-27.
+ */
+
+public class DebugLog {
+}
